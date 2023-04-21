59f1c0a527d73c2e6f7010bf1721ff1ffd74d531
nastasiav <baeva.asya@gmail.com>
2023-04-21 18:41:48 +0300

    Initial commit

---
+++ src/main/java/my/edu/Netology/RandomArray.java
@@ -0,0 +1,15 @@
+package my.edu.Netology;
+
+import java.util.Random;
+
+public class RandomArray {
+    private Random random = new Random();
+    public Integer[] setRandomIntegerArray(int lengthArray){
+        Integer[] array = new Integer[lengthArray];
+        for (int i = 0; i < array.length; i++) {
+            int rand = random.nextInt(1000);
+            array[i] = rand;
+        }
+        return array;
+    }
+}
---
+++ src/main/java/my/edu/Netology/lesson1/cw/w1/Main.java
@@ -0,0 +1,41 @@
+package my.edu.Netology.lesson1.cw.w1;
+
+import my.edu.Netology.RandomArray;
+
+/**
+ * Алгоритм. Сортировка массива - обычная
+ */
+public class Main {
+    private static final int ARRAY_LENGTH = 10;
+    private static final boolean ASC = true;
+    private static final boolean DESC = false;
+
+    public static void main(String[] args) {
+        Integer[] array = new RandomArray().setRandomIntegerArray(ARRAY_LENGTH);
+
+        myPrintArray(array);
+        mySortArray(array, ASC);
+        myPrintArray(array);
+        mySortArray(array, DESC);
+        myPrintArray(array);
+    }
+
+    private static void myPrintArray(Integer[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    private static void mySortArray(Integer[] array, boolean asc) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (i != j && ((array[i] < array[j] && asc) || (array[i] > array[j] && !asc))) {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+}
