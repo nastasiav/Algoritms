1f53dd8aca01ce1d7ec0b9c52d82858baa59427e
nastasiav <baeva.asya@gmail.com>
2023-05-11 00:14:09 +0300

    Lesson 1. C-Work 3

--- src/main/java/my/edu/Netology/lesson1/cw/w3/Main.java
+++ src/main/java/my/edu/Netology/lesson1/cw/w3/Main.java
@@ -1,4 +1,44 @@
 package my.edu.Netology.lesson1.cw.w3;
 
+import my.edu.Netology.RandomArray;
+
 public class Main {
+    private static final int SIZE = 15;
+    public static void main(String[] args) {
+        my.edu.Netology.lesson1.cw.w1.Main mainW1 = new my.edu.Netology.lesson1.cw.w1.Main();
+
+        Integer[] randomArray = new RandomArray().setRandomIntegerArray(SIZE);
+        mainW1.myPrintArray(randomArray);
+
+        Integer maxEl = getMaxElement(randomArray);
+        int maxIndex = getMaxIndex(randomArray);
+
+        System.out.println(maxEl);
+        System.out.println(maxIndex);
+    }
+
+    private static int getMaxIndex(Integer[] randomArray) {
+        int maxIndex = 0;
+        int max = randomArray[0];
+
+        for (int i = 1; i < randomArray.length; i++) {
+            if (randomArray[i] > max) {
+                maxIndex = i;
+                max = randomArray[i];
+            }
+        }
+
+        return maxIndex;
+    }
+
+    private static Integer getMaxElement(Integer[] randomArray) {
+        int max = randomArray[0];
+
+        for (int i : randomArray) {
+            if (max < i)
+                max = i;
+        }
+
+        return max;
+    }
 }
